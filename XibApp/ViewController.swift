4c1cbb306574cd8ebf029cbc84ff711d0493d8f1
Francisco Sena <Franciscosenarivas@gmail.com>
2022-07-05 19:40:19 +0200

    Initial Commit

---
+++ XibApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XibApp
+//
+//  Created by Francisco Sena Rivas on 5/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
